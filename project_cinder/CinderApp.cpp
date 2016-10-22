1528de019dc04093f2cc83fbead18a93329e3ead
victorburckel <victor.burckel@gmail.com>
2016-10-22 13:19:46 +0200

    Initial empty project creation

---
+++ project_cinder/CinderApp.cpp
@@ -0,0 +1,13 @@
+#include <cinder/app/App.h>
+#include <cinder/app/RendererGl.h>
+
+namespace {
+
+class CinderApp : public ci::app::App
+{
+
+};
+
+}
+
+CINDER_APP( CinderApp, ci::app::RendererGl )
